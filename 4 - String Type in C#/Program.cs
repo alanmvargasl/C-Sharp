a3a8be54fdb70fa04082d7e4b2a0c9d494897eff
alanmvargas <alanvargas@devgama.com>
2019-10-04 13:27:14 -0500

    String Values
    - It teaches how to capture data into a string variable and it also shows how to display it on the console

---
+++ 4 - String Type in C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4___String_Type_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alan";
+            string lastName = "Vargas";
+            Console.WriteLine("Your First Name is {0} and your Last Name is {1}", name, lastName );
+
+        }
+    }
+}
