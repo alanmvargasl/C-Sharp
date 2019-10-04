b57626dbdb3325ad04ae5305740778279b4fc4d9
alanmvargas <alanvargas@devgama.com>
2019-10-04 18:04:12 -0500

    Do While Statement

---
+++ 14 - Do While Loop in C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _14___Do_While_Loop_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value");
+            int value = int.Parse(Console.ReadLine());
+            do{
+                Console.WriteLine("I will do this once and then i will verify if i keep looping or not!");
+            }while(value == 1);
+        }
+    }
+}
