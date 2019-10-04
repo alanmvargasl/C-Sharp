f505813aa20fbc08b54fe1acfc875b62b7fda018
alanmvargas <alanvargas@devgama.com>
2019-10-04 16:37:42 -0500

    Common Operators

---
+++ 5 - Common Operators in C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _5___Common_Operators_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#region Arithmetic Operators
+/* 
++	Adds two operands	A + B = 30
+-	Subtracts second operand from the first	A - B = -10
+*	Multiplies both operands	A * B = 200
+/	Divides numerator by de-numerator	B / A = 2
+%	Modulus Operator and remainder of after an integer division	B % A = 0
+++	Increment operator increases integer value by one	A++ = 11
+--	Decrement operator decreases integer value by one	A-- = 9
+*/
+#endregion
+
+#region Relational Operators
+/*
+==	Checks if the values of two operands are equal or not, if yes then condition becomes true.	(A == B) is not true.
+!=	Checks if the values of two operands are equal or not, if values are not equal then condition becomes true.	(A != B) is true.
+>	Checks if the value of left operand is greater than the value of right operand, if yes then condition becomes true.	(A > B) is not true.
+<	Checks if the value of left operand is less than the value of right operand, if yes then condition becomes true.	(A < B) is true.
+>=	Checks if the value of left operand is greater than or equal to the value of right operand, if yes then condition becomes true.	(A >= B) is not true.
+<=	Checks if the value of left operand is less than or equal to the value of right operand, if yes then condition becomes true.	(A <= B) is true.
+ */
+#endregion
+
+#region Logical Operators
+/*
+&&	Called Logical AND operator. If both the operands are non zero then condition becomes true.	(A && B) is false.
+||	Called Logical OR Operator. If any of the two operands is non zero then condition becomes true.	(A || B) is true.
+!	Called Logical NOT Operator. Use to reverses the logical state of its operand. If a condition is true then Logical NOT operator will make false.	!(A && B) is true.
+*/
+
+#endregion
+
+
+        }
+    }
+}
