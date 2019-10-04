64bc1282e260720489f6eeb33737567b087fa388
alanmvargas <alanvargas@devgama.com>
2019-10-04 17:56:05 -0500

    While Statement

---
+++ 13 - While Loop in C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _13___While_Loop_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number");
+            int number = int.Parse(Console.ReadLine());
+            int i =0;
+            while (number == 20)
+            {
+                i++;
+                Console.WriteLine("This is lap" + i);
+                if (i==100){ number =21;}
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
