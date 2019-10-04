8071f9057f2a7178d98ed306774caeed329f4ab9
alanmvargas <alanvargas@devgama.com>
2019-10-04 13:13:02 -0500

    Reading and Writing Values

---
+++ 2 - Reading And Writing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2___Reading_And_Writing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a value");
+            string value = Console.ReadLine();
+            Console.WriteLine("Your Value is:  " + value);
+        }
+    }
+}
