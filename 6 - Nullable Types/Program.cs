0d0ee1339b05411f24bda33c3b000783e0cdc611
alanmvargas <alanvargas@devgama.com>
2019-10-04 16:57:35 -0500

    Coalescing and Null Coalescing Operator

---
+++ 6 - Nullable Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6___Nullable_Types
+{ //Coalescing Operator and N
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int Age = 24;
+
+            //Coalescing Operator
+            bool myAnswer = Age >= 18 ? true:false;
+            Console.WriteLine("Is Alan An ADULT? " + myAnswer);
+
+            //Null Coalescing Operator ??
+            int? Age1 = null;
+            int isTheValueNull = Age1 ?? 0;
+            Console.WriteLine(isTheValueNull);
+        }
+    }
+}
