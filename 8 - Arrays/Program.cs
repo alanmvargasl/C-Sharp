8a9dbc7a8d1bae0442052a5bde44c15199a47da0
alanmvargas <alanvargas@devgama.com>
2019-10-04 17:14:32 -0500

    Arrays

---
+++ 8 - Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _8___Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How manny spaces do you want for the array?");
+            int arraySpace = int.Parse(Console.ReadLine());
+            int[] evenNumbers = new int[arraySpace];
+            int counter=2;
+            for (int i=0; i<arraySpace;i++)
+            {
+                evenNumbers[i] = counter;
+                Console.WriteLine(evenNumbers[i]);
+                counter = counter + 2;
+            } 
+            Console.WriteLine(evenNumbers.Length);
+        }
+    }
+}
