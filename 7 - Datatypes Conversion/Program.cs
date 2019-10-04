c2954d13011755400d4c1644f2ac31048da813ed
alanmvargas <alanvargas@devgama.com>
2019-10-04 17:03:45 -0500

    Datatypes Conversions

---
+++ 7 - Datatypes Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _7___Datatypes_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strNumber = "100447";
+            int result=0;
+            bool isConversionSuccessful = int.TryParse(strNumber,out result);
+
+            if (isConversionSuccessful)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.Write("Please Enter a Valid Number");
+            }
+        }
+    }
+}
