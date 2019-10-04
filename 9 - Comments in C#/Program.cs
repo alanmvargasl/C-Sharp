6e4c878b6bd525b3dbb084317daf8dcb67eea6b0
alanmvargas <alanvargas@devgama.com>
2019-10-04 17:16:52 -0500

    Comments in C#

---
+++ 9 - Comments in C#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _9___Comments_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //This video is about commenting the code
+           //You can comment by Line with the }"//"
+           /* 
+           Or you could do a lot of lines without 
+           putting the "//" in every line...
+           this works well for paragraphs or explicit
+           information!
+
+           Alt + Shift + A = Comments all things selected.
+           
+            */
+        }
+    }
+}
