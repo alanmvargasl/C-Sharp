88e18b7f418281b2fd8c6a3a26fed0ebc5d9a99f
alanmvargas <alanvargas@devgama.com>
2019-10-04 13:18:05 -0500

    Built In Types
    -It explains the most used type of variables that we can use in CSharp

---
+++ 3 - Built In Types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3___Built_In_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = 0;
+            char chain = NULL;
+            short myShortValue = 1;
+            long myLongValue= 12343564334;
+            float myFloat = 12.33;
+            double myDouble = 34453224.53;
+            string myString ="This Contains a set of Characters!";
+        }
+    }
+}
