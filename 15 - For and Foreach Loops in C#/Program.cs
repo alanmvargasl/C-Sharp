54b1d744262f4ef251189b4381d5c01c76fe0096
alanmvargas <alanvargas@devgama.com>
2019-10-04 18:25:43 -0500

    For and Foreach Statement

---
+++ 15 - For and Foreach Loops in C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _15___For_and_Foreach_Loops_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("1 -  Even Numbers \n 2 -  Odd Numbers");
+        
+        int increment = 2;
+        int option = int.Parse(Console.ReadLine());
+        switch (option)
+        {   
+            case 1:
+                int number = 20;
+                for (int i=0;i<number;i = i + 2)
+                {
+                    Console.WriteLine("Even Numbers " + i);
+                }
+                break;
+            case 2: 
+                int number1 =20;
+                for (int i=1;i<number1;i = i +2)
+                {
+                    Console.WriteLine("Odd Numbers "+ i);
+                }
+                break;
+            default: Console.WriteLine("Invalid Option");
+            break;
+        }
+        }
+    }
+}
