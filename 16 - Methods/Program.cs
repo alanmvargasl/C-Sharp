2669730a22865f0be6c6320a7964ec5adcd4fefa
alanmvargas <alanvargas@devgama.com>
2019-10-08 19:56:23 -0500

    Methods

---
+++ 16 - Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace _16___Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(addMethod(2,4));
+           Console.WriteLine(substractMethod(33,43));
+        }
+
+        static int addMethod (int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        static int substractMethod (int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+    }
+}
