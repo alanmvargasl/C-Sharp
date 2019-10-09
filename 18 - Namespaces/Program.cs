a864c75fb9eeaf34a8029c5b46329db201c0896f
alanmvargas <alanvargas@devgama.com>
2019-10-08 20:03:10 -0500

    Namespaces

---
+++ 18 - Namespaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using new_namespace; //This tutorial explains how to create a namespace and use it in other programs.
+
+namespace _18___Namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
