d84cbd91c60d6fc8907cf4f4148868de0ecae2db
alanmvargas <alanvargas@devgama.com>
2019-10-14 11:00:48 -0500

    Method Hiding in C#

---
+++ 22 - Method Hiding in C#/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _22___Method_Hiding_in_C_
+{
+
+    public class Employee
+    {
+        public string FirstName;
+        public string Lastname;
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + Lastname);
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        public new void PrintFullName()
+        {
+            base.PrintFullName();
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.FirstName = "Alan";
+            FTE.Lastname = "Vargas";
+            FTE.PrintFullName();
+
+            PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.FirstName = "Part Time";
+            PTE.Lastname = "Employee";
+            PTE.PrintFullName();
+        }
+    }
+}
