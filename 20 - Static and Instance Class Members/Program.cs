dbc2b08957555dabe620cb980329814a0d71b620
alanmvargas <alanvargas@devgama.com>
2019-10-14 10:39:46 -0500

    Static and Instance Class Members

---
+++ 20 - Static and Instance Class Members/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _20___Static_and_Instance_Class_Members
+{
+    class Circle
+    {
+        static float _PI = 3.141F;
+        int _Radius;
+
+        public Circle(int Radius)
+        {
+            this._Radius=Radius;
+        }
+
+        public float CalculateArea()
+        {
+            return Circle._PI * this._Radius * this._Radius;
+        }
+    }
+    
+    
+    
+
+
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
