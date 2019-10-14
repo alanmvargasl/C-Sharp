427977d111a15233c232908ecf153d7a7a46028e
alanmvargas <alanvargas@devgama.com>
2019-10-14 10:54:42 -0500

    Inheritance in C#

---
+++ 21 - Inheritance in C#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _21___Inheritance_in_C_
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+    
+
+    public class FullTimeEmployee : Employee
+    {
+        public float YearlySalary;
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
