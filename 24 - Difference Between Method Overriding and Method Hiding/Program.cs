7eef38c25ded4a13087c384e184645f9b1b71a35
alanmvargas <alanvargas@devgama.com>
2019-10-19 19:02:14 -0500

    Polymorphism

---
+++ 24 - Difference Between Method Overriding and Method Hiding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _24___Difference_Between_Method_Overriding_and_Method_Hiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
