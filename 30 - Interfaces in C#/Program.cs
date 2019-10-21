73e427f533a6add0258d0db7e9000b649f5e884d
alanmvargas <alanvargas@devgama.com>
2019-10-21 18:15:26 -0500

    Interfaces in C#

---
+++ 30 - Interfaces in C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _30___Interfaces_in_C_
+{
+    interface ICostumer
+    {
+        void Print(); //You Cannot Implement Anything here
+        //Just Declarations are accepted!
+    }
+
+    interface ICostumer2
+    {
+        void Print2(); //Same Here
+    }
+
+    class Customer : ICostumer, ICostumer2
+    { //All the implementations are defined here
+    //A Class Can Inherit more than one Interface
+        public void Print()
+        {
+            Console.WriteLine("This is Interface Print Method1");
+        }
+        public void Print2()
+        {
+            Console.WriteLine("This is Interface Print Method2");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer C1 = new Customer();
+            C1.Print();
+            C1.Print2();
+        }
+    }
+}
