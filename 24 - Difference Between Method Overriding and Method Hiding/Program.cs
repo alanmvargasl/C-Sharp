42cf2f22838fe5f2347127dfb6db8cc081091ce8
alanmvargas <alanvargas@devgama.com>
2019-10-21 14:02:08 -0500

    24 - Difference Between Method Overriding and Method Hiding

--- 24 - Difference Between Method Overriding and Method Hiding/Program.cs
+++ 24 - Difference Between Method Overriding and Method Hiding/Program.cs
@@ -2,11 +2,30 @@
 
 namespace _24___Difference_Between_Method_Overriding_and_Method_Hiding
 {
+
+    public class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("I am a Base Class Method");
+        }
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public new void Print ()
+        {
+            Console.WriteLine("I am a Derived Class Method");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BaseClass NEWBASECLASS = new DerivedClass();
+            NEWBASECLASS.Print();
         }
     }
 }
