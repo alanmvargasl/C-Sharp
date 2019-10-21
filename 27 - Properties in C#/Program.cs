e13252d041a2f4db82cb0eb3da6cd2c15d347273
alanmvargas <alanvargas@devgama.com>
2019-10-21 14:58:44 -0500

    Properties in C#

---
+++ 27 - Properties in C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _27___Properties_in_C_
+{
+    public class ME
+    {
+        private int age;
+
+        public int setAge
+        {
+            set { if (value <= 0)
+            {
+            Console.WriteLine("Please type a number bigger than 0");
+            }
+            this.age = value;
+            }
+            get {
+               return this.age;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ME alansage = new ME();
+            alansage.setAge = 0;
+            Console.WriteLine(alansage.setAge) ;
+        }
+    }
+}
