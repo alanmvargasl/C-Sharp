f1917b2fa16e5be10956815b070d865ab6ccddc3
alanmvargas <alanvargas@devgama.com>
2019-10-23 16:50:26 -0500

    Abstract classes in c#

--- 32 - Abstract Classes in C#/Program.cs
+++ 32 - Abstract Classes in C#/Program.cs
@@ -2,11 +2,23 @@
 
 namespace _32___Abstract_Classes_in_C_
 {
-    class Program
+    public abstract class Customer
     {
+        public abstract void Print();
+    }
+
+    class Program : Customer
+    {
+
+        public override void Print()
+        {
+            Console.WriteLine("This is the implementation for the Abstract inherited method");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer C1 = new Program();
+            C1.Print();
         }
     }
 }
