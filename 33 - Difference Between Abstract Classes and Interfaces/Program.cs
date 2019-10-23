d315d0fb3f9b070400a9c39385632e735841c8de
alanmvargas <alanvargas@devgama.com>
2019-10-23 17:00:58 -0500

    Difference between abstract classes and interfaces

---
+++ 33 - Difference Between Abstract Classes and Interfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _33___Difference_Between_Abstract_Classes_and_Interfaces
+{
+
+    public abstract class Customer
+    {
+        //An Abstract class can have fields, can have implementations for some of its members (methods) and interfaces cant.
+        //An interface can inherit from multiple interfaces... an abstract class Cannot.
+        int ID;
+        public void Print()
+        {
+            Console.WriteLine("Print");        
+        }
+            
+    }
+
+    public interface ICostumer
+    {
+        void Print();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
