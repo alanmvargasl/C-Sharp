04f0c7d2c22c1af0cb9e9338fbf3bdd6b15638b8
alanmvargas <alanvargas@devgama.com>
2019-10-28 13:45:34 -0600

    Multicast Delegates in C# 2

---
+++ 39 - Multicast Delegates in C#/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _38___Delegates_usage_in_c__continued
+{
+    //This is pretty cool, we can use delegates to invoke 1 or more methods at the same time.
+    //This delegates are called MULTICAST DELEGATES
+    public delegate void myDelegate();
+    
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            myDelegate example = new myDelegate(myMethod1);
+            example += myMethod2;
+            example += myMethod3;
+            example();
+        }
+
+        public static void myMethod1()
+        {
+            Console.WriteLine("Method 1 Invoked");    
+        }
+
+        public static void myMethod2()
+        {
+            Console.WriteLine("Method 2 Invoked");    
+        }
+
+        public static void myMethod3()
+        {
+            Console.WriteLine("Method 3 Invoked");    
+        }
+
+    }
+}
