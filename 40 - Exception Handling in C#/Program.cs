7eb65efbf415f1c27268d5d70438e0cf616f42f1
alanmvargas <alanvargas@devgama.com>
2019-10-28 14:27:57 -0600

    Exceptions in C#, Very Very Important a Useful!

---
+++ 40 - Exception Handling in C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _40___Exception_Handling_in_C_
+{
+    
+    //Awesome, very important!
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try{
+           StreamReader reader = new StreamReader(@"C:\Github\C-Sharp\40 - Exception Handling in C#\Data.txt");
+           Console.WriteLine(reader.ReadToEnd());
+           reader.Close();
+           }
+           catch(FileNotFoundException error){
+            Console.WriteLine("Your File in File Location {0} was not found", error.FileName);
+           }
+           catch(DirectoryNotFoundException error){
+            Console.WriteLine(error.Message);
+           }
+          
+        }
+    }
+}
